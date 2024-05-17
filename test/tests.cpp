ce1d67266814cf6d2f8f8f1e526ec3ddc710ccbe
maxWoosh <140332381+maxWoosh@users.noreply.github.com>
2024-05-17 12:13:34 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -2,7 +2,7 @@
 #include "Automata.h"
 #include <gtest/gtest.h>
 
-class AutomataTest : public ::testing::Test {
+class ATest : public ::testing::Test {
  protected:
     Automata automata;
 
@@ -11,54 +11,54 @@ class AutomataTest : public ::testing::Test {
     }
 };
 
-TEST_F(AutomataTest, IsInitiallyOff) {
+TEST_F(ATest, 1) {
     Automata newAutomata;
     EXPECT_EQ(newAutomata.getState(), STATES::OFF);
 }
 
-TEST_F(AutomataTest, TurnsOn) {
+TEST_F(ATest, 2) {
     EXPECT_EQ(automata.getState(), STATES::WAIT);
 }
 
-TEST_F(AutomataTest, AcceptsCoins) {
-    automata.coin(100);
+TEST_F(ATest, 3) {
+    automata.coin(50);
     EXPECT_EQ(automata.getState(), STATES::ACCEPT);
 }
 
-TEST_F(AutomataTest, CanReadMenu) {
+TEST_F(ATest, 4) {
     EXPECT_NO_THROW(automata.getMenu());
 }
 
-TEST_F(AutomataTest, MakesSelection) {
-    automata.coin(200);
+TEST_F(ATest, 5) {
+    automata.coin(100);
     automata.choice(2);
     EXPECT_EQ(automata.getState(), STATES::WAIT);
 }
 
-TEST_F(AutomataTest, ChecksSufficientFunds) {
-    automata.coin(20);
+TEST_F(ATest, 6) {
+    automata.coin(10);
     EXPECT_FALSE(automata.check(1));
 }
 
-TEST_F(AutomataTest, CancelsTransaction) {
-    automata.coin(100);
+TEST_F(ATest, 7) {
+    automata.coin(50);
     automata.cancel();
     EXPECT_EQ(automata.getState(), STATES::WAIT);
 }
 
-TEST_F(AutomataTest, CooksDrink) {
-    automata.coin(200);
+TEST_F(ATest, 8) {
+    automata.coin(100);
     automata.choice(1);
     EXPECT_EQ(automata.getState(), STATES::WAIT);
 }
 
-TEST_F(AutomataTest, FinishesTransaction) {
-    automata.coin(200);
+TEST_F(ATest, 9) {
+    automata.coin(100);
     automata.choice(1);
     EXPECT_EQ(automata.getState(), STATES::WAIT);
 }
 
-TEST_F(AutomataTest, DoesNotCookWithoutFunds) {
+TEST_F(ATest, 10) {
     automata.choice(1);
     EXPECT_NE(automata.getState(), STATES::COOK);
 }
