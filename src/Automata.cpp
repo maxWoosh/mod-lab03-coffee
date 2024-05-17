a9ff4065582c82a69313c7a12a3a92a5a1c83b2c
maxWoosh <140332381+maxWoosh@users.noreply.github.com>
2024-05-17 11:40:13 +0300

    Update Automata.cpp

--- src/Automata.cpp
+++ src/Automata.cpp
@@ -1,1 +1,82 @@
+// Copyright 2022 UNN-IASR
+#include "Automata.h"
+#include <iostream>
 
+Automata::Automata() : cash(0), state(STATES::OFF) {
+    menu = { "Tea", "Coffe", "Cacao" };
+    price = { 50, 100, 60 };
+}
+
+void Automata::on() {
+    if (state == STATES::OFF) {
+        state = STATES::WAIT;
+        std::cout << "On!" << std::endl;
+    }
+}
+
+void Automata::off() {
+    if (state == STATES::WAIT) {
+        state = STATES::OFF;
+        std::cout << "Off!" << std::endl;
+    }
+}
+
+void Automata::coin(int Money) {
+    if (state == STATES::WAIT || state == STATES::ACCEPT) {
+        cash += Money;
+        state = STATES::ACCEPT;
+        std::cout << "Balance: " << cash << std::endl;
+    }
+}
+
+void Automata::getMenu() {
+    std::cout << "Menu:" << std::endl;
+    for (size_t i = 0; i < menu.size(); ++i) {
+        std::cout << i + 1 << ". ";
+        std::cout << menu[i] << " - ";
+        std::cout << price[i] << std::endl;
+    }
+}
+
+STATES Automata::getState() {
+    return state;
+}
+
+void Automata::choice(unsigned int Num) {
+    if (state == STATES::ACCEPT && Num <= menu.size()) {
+        state = STATES::CHECK;
+        if (check(Num - 1)) {
+            cook(Num - 1);
+        }
+        else {
+            std::cout << "Not enough money!" << std::endl;
+            cancel();
+        }
+    }
+}
+
+bool Automata::check(unsigned int Num) {
+    return cash >= price[Num];
+}
+
+void Automata::cancel() {
+    if (state == STATES::ACCEPT || state == STATES::CHECK) {
+        std::cout << "Take your money, pls" << cash << std::endl;
+        cash = 0;
+        state = STATES::WAIT;
+    }
+}
+
+void Automata::cook(unsigned int drinkNumber) {
+    if (state == STATES::CHECK) {
+        std::cout << "Cooking... " << menu[Num] << std::endl;
+        cash -= price[drinkNumber];
+        state = STATES::COOK;
+        finish();
+    }
+}
+
+void Automata::finish() {
+    std::cout << "Take your drink pls!" << std::endl;
+    state = STATES::WAIT;
+}
