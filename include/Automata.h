fa54247024ae9e98964607b8164604adb934fbc6
maxWoosh <140332381+maxWoosh@users.noreply.github.com>
2024-05-17 11:39:32 +0300

    Update Automata.h

--- include/Automata.h
+++ include/Automata.h
@@ -1,1 +1,33 @@
+// Copyright 2022 UNN-IASR
+#pragma once
+#include <string>
+#include <vector>
 
+enum class STATES {
+    OFF,
+    WAIT,
+    ACCEPT,
+    CHECK,
+    COOK
+};
+
+class Automata {
+private:
+    int cash;
+    std::vector<std::string> menu;
+    std::vector<int> price;
+    STATES state;
+
+public:
+    Automata();
+    void on();
+    void off();
+    void coin(int Money);
+    void getMenu();
+    STATES getState();
+    void choice(int Num);
+    bool check(int Num);
+    void cancel();
+    void cook(int Num);
+    void finish();
+};
