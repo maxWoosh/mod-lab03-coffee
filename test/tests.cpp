403f596098f7ce49ec1cc2a92eee3fc08f6816ee
maxWoosh <140332381+maxWoosh@users.noreply.github.com>
2024-05-17 11:55:21 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -2,63 +2,63 @@
 #include "Automata.h"
 #include <gtest/gtest.h>
 
-class ATest : public ::testing::Test {
+class AutomataTest : public ::testing::Test {
 protected:
-    Automata vendingMachine;
+    Automata automata;
 
     void SetUp() override {
-        vendingMachine.on();
+        automata.on();
     }
 };
 
-TEST(ATest, IsInitiallyOff) {
+TEST_F(AutomataTest, IsInitiallyOff) {
     Automata newAutomata;
     EXPECT_EQ(newAutomata.getState(), STATES::OFF);
 }
 
-TEST(Atest, TurnsOn) {
-    EXPECT_EQ(vendingMachine.getState(), STATES::WAIT);
+TEST_F(AutomataTest, TurnsOn) {
+    EXPECT_EQ(automata.getState(), STATES::WAIT);
 }
 
-TEST(Atest, AcceptsCoins) {
-    vendingMachine.coin(50);
-    EXPECT_EQ(vendingMachine.getState(), STATES::ACCEPT);
+TEST_F(AutomataTest, AcceptsCoins) {
+    automata.coin(100);
+    EXPECT_EQ(automata.getState(), STATES::ACCEPT);
 }
 
-TEST(Atest, CanReadMenu) {
-    EXPECT_NO_THROW(vendingMachine.getMenu());
+TEST_F(AutomataTest, CanReadMenu) {
+    EXPECT_NO_THROW(automata.getMenu());
 }
 
-TEST(Atest, MakesSelection) {
-    vendingMachine.coin(200);
-    vendingMachine.choice(2);
-    EXPECT_EQ(vendingMachine.getState(), STATES::WAIT);
+TEST_F(AutomataTest, MakesSelection) {
+    automata.coin(200);
+    automata.choice(2);
+    EXPECT_EQ(automata.getState(), STATES::WAIT);
 }
 
-TEST(Atest, ChecksSufficientFunds) {
-    vendingMachine.coin(10);
-    EXPECT_FALSE(vendingMachine.check(1));
+TEST_F(AutomataTest, ChecksSufficientFunds) {
+    automata.coin(20);
+    EXPECT_FALSE(automata.check(1));
 }
 
-TEST(Atest, CancelsTransaction) {
-    vendingMachine.coin(100);
-    vendingMachine.cancel();
-    EXPECT_EQ(vendingMachine.getState(), STATES::WAIT);
+TEST_F(AutomataTest, CancelsTransaction) {
+    automata.coin(100);
+    automata.cancel();
+    EXPECT_EQ(automata.getState(), STATES::WAIT);
 }
 
-TEST(Atest, CooksDrink) {
-    vendingMachine.coin(200);
-    vendingMachine.choice(1);
-    EXPECT_EQ(vendingMachine.getState(), STATES::WAIT);
+TEST_F(AutomataTest, CooksDrink) {
+    automata.coin(200);
+    automata.choice(1);
+    EXPECT_EQ(automata.getState(), STATES::WAIT);
 }
 
-TEST(Atest, FinishesTransaction) {
-    vendingMachine.coin(200);
-    vendingMachine.choice(1);
-    EXPECT_EQ(vendingMachine.getState(), STATES::WAIT);
+TEST_F(AutomataTest, FinishesTransaction) {
+    automata.coin(200);
+    automata.choice(1);
+    EXPECT_EQ(automata.getState(), STATES::WAIT);
 }
 
-TEST(Atest, DoesNotCookWithoutFunds) {
-    vendingMachine.choice(1);
-    EXPECT_NE(vendingMachine.getState(), STATES::COOK);
+TEST_F(AutomataTest, DoesNotCookWithoutFunds) {
+    automata.choice(1);
+    EXPECT_NE(automata.getState(), STATES::COOK);
 }
