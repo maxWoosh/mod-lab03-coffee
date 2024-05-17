07bc6c8109ec796e4a56f41cad71708327216170
maxWoosh <140332381+maxWoosh@users.noreply.github.com>
2024-05-17 11:40:30 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,13 @@
+// Copyright 2022 UNN-IASR
+#include "Automata.h"
 
+int main() {
+    Automata vendingMachine;
+    vendingMachine.on();
+    vendingMachine.getMenu();
+    vendingMachine.coin(100);
+    vendingMachine.choice(2);
+    vendingMachine.off();
+
+    return 0;
+}
