82ef8f5b3af6384264069ab3f480b04fdeb83e51
maxWoosh <140332381+maxWoosh@users.noreply.github.com>
2024-05-17 11:40:56 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,4 +1,64 @@
-// Copyright 2022 GHA Test Team
-
-#include <gtest/gtest.h>
+// Copyright 2022 UNN-IASR
 #include "Automata.h"
+#include <gtest/gtest.h>
+
+class ATest : public ::testing::Test {
+protected:
+    Automata vendingMachine;
+
+    void SetUp() override {
+        vendingMachine.on();
+    }
+};
+
+TEST(ATest, IsInitiallyOff) {
+    Automata newAutomata;
+    EXPECT_EQ(newAutomata.getState(), STATES::OFF);
+}
+
+TEST(Atest, TurnsOn) {
+    EXPECT_EQ(vendingMachine.getState(), STATES::WAIT);
+}
+
+TEST(Atest, AcceptsCoins) {
+    vendingMachine.coin(50);
+    EXPECT_EQ(vendingMachine.getState(), STATES::ACCEPT);
+}
+
+TEST(Atest, CanReadMenu) {
+    EXPECT_NO_THROW(vendingMachine.getMenu());
+}
+
+TEST(Atest, MakesSelection) {
+    vendingMachine.coin(200);
+    vendingMachine.choice(2);
+    EXPECT_EQ(vendingMachine.getState(), STATES::WAIT);
+}
+
+TEST(Atest, ChecksSufficientFunds) {
+    vendingMachine.coin(10);
+    EXPECT_FALSE(vendingMachine.check(1));
+}
+
+TEST(Atest, CancelsTransaction) {
+    vendingMachine.coin(100);
+    vendingMachine.cancel();
+    EXPECT_EQ(vendingMachine.getState(), STATES::WAIT);
+}
+
+TEST(Atest, CooksDrink) {
+    vendingMachine.coin(200);
+    vendingMachine.choice(1);
+    EXPECT_EQ(vendingMachine.getState(), STATES::WAIT);
+}
+
+TEST(Atest, FinishesTransaction) {
+    vendingMachine.coin(200);
+    vendingMachine.choice(1);
+    EXPECT_EQ(vendingMachine.getState(), STATES::WAIT);
+}
+
+TEST(Atest, DoesNotCookWithoutFunds) {
+    vendingMachine.choice(1);
+    EXPECT_NE(vendingMachine.getState(), STATES::COOK);
+}
